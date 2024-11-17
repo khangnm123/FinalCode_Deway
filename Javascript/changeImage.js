56b73e7db73698c8aa997b5bd0d1c5b7aba3c008
khangnm123 <khangpro2536@gmail.com>
2024-11-17 17:26:22 +0700

    first commit

---
+++ Javascript/changeImage.js
@@ -0,0 +1,21 @@
+
+let change = document.getElementById("change");
+let change1 = document.getElementById("change1");
+let change2 = document.getElementById("change2");
+let change3 = document.getElementById("change2");
+let change4 = document.getElementById("change4");
+function changeImage(){
+    change.src="./image/Home/chính.png";
+    change1.src="./image/Home/fully___global_1730792713_3494465011245009065_65130945375.jpg";
+    change2.src="./image/Home/fully___global_1730792713_3494465011245009065_65130945375.jpg";
+    change3.src="./image/Home/fully___global_1730792713_3494465011245009065_65130945375.jpg";
+    change4.src="./image/Home/fully___global_1730792713_3494465011245009065_65130945375.jpg";
+}
+
+
+
+
+let MenuClick1 = document.getElementById("on");
+function MenuClick(){
+    MenuClick1.classList.toggle("active");
+}
